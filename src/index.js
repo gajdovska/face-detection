657c3f659d1f9d2154d0bbcfe610a7e3aa726994
gajdovska <sara.gajdovskaa@gmail.com>
2020-02-29 16:21:21 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -10,8 +10,3 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister(); nene builds ne mora na github, bi trebalo ignore da ti e toa, najtipicen nacin e, se postavuva pipe, kade sho
-mozes od heroku primer da reces, 
-// ima nova verzija na stranicava, zemi gi promenite
-// izbildaj lokalno na serverot i smeni go staroto so novoto
-// racno ne treba da prikacuvas raboti i siea od koa go naprai toa build sjo ke rpraime ?
