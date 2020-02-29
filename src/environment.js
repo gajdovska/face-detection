1364f74d1eef546e9a5b8247b832f4f585477189
gajdovska <sara.gajdovskaa@gmail.com>
2020-02-29 18:20:58 +0100

    bug fix

--- src/environment.js
+++ src/environment.js
@@ -2,10 +2,10 @@ const environment = {};
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
     // dev code
-    environment.apiURL = 'https://face-detection-apii.herokuapp.com';
-} else {
-    // production code\
     environment.apiURL = 'http://localhost:3000';
+} else {
+    // production code
+    environment.apiURL = 'https://face-detection-apii.herokuapp.com';
 }
 
 export default environment;
